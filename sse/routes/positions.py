6dbc9d72e9580219c300e52ce2aec1b41117a109
João <joao_bernardi@outlook.com>
2023-08-11 10:45:11 -0300

    `SSE` - Unregister handler after disconnect

--- sse/routes/positions.py
+++ sse/routes/positions.py
@@ -2,28 +2,33 @@ from fastapi import Query
 from fastapi.routing import APIRouter
 from fastapi.responses import StreamingResponse
 import pyding
+from pyding.structures import QueuedHandler
 import json
 from queue import Queue, Empty
 from typing import List
 import logging
+import fastapi
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix='/positions')
 
 @router.get('/')
-async def get_positions(tracker: List[int] = Query(None)):
-    def queue_positions():
-        logger.info(tracker)
-        if tracker:
-            queue: Queue = pyding.queue('position.message', tracker_id=pyding.Contains(tracker))
-        else:
-            queue: Queue = pyding.queue('position.message')
-
+async def get_positions(background_tasks: fastapi.background.BackgroundTasks, tracker: List[int] = Query(None)):
+    # Setup handler
+    if tracker:
+        handler: QueuedHandler = pyding.queue('position.message', tracker_id=pyding.Contains(tracker))
+    else:
+        handler: QueuedHandler = pyding.queue('position.message', tracker_id=pyding.Contains(tracker))
+    queue: Queue = handler.get_queue()
+    background_tasks.add_task(lambda h: h.unregister(), handler)
+    
+    def queue_positions(queue):
         while True:
             try:
                 data = queue.get(timeout=5)
                 yield f"id: {data['id']}\nevent: position_update\ndata: {json.dumps({'type': 1, 'data': data['message']})}\n\n"
             except Empty:
                 yield 'id: -1\nevent: keep-alive\ndata: {}\n\n'
-    return StreamingResponse(queue_positions(), media_type="text/event-stream")
+
+    return StreamingResponse(queue_positions(queue), media_type="text/event-stream")
