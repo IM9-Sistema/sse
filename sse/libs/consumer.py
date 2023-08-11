439997ae7688909702c724388a8ee41f8beb464e
João <joao_bernardi@outlook.com>
2023-08-11 10:50:48 -0300

    `SSE` - Use env

--- sse/libs/consumer.py
+++ sse/libs/consumer.py
@@ -3,7 +3,7 @@ from os import environ
 import json
 
 async def consume_from_topic(*topics):
-    consumer = AIOKafkaConsumer(*topics, bootstrap_servers='10.10.1.18:9092', value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+    consumer = AIOKafkaConsumer(*topics, bootstrap_servers=environ['KAFKA_ADDRESS'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
     await consumer.start()
     async for message in consumer:
         
