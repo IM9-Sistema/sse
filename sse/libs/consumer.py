3fc6c631c070c5cab92adb7994c801126dfb9417
João <joao_bernardi@outlook.com>
2023-09-04 08:43:43 -0300

    `Consumer` - Retry connection

--- sse/libs/consumer.py
+++ sse/libs/consumer.py
@@ -3,8 +3,12 @@ from os import environ
 import json
 
 async def consume_from_topic(*topics):
-    consumer = AIOKafkaConsumer(*topics, bootstrap_servers=environ['KAFKA_ADDRESS'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-    await consumer.start()
-    async for message in consumer:
-        
-        yield message.value, message.offset
+    while True:
+        try:
+            consumer = AIOKafkaConsumer(*topics, bootstrap_servers=environ['KAFKA_ADDRESS'], value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+            await consumer.start()
+            async for message in consumer:
+                
+                yield message.value, message.offset
+        except:
+            continue
