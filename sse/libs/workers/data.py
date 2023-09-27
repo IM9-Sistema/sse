36a78bf7acef2eb5262f75877ad58307c1496d0e
Jão <joao_bernardi@outlook.com>
2023-09-27 11:13:38 -0300

    `Data` - Try catch

--- sse/libs/workers/data.py
+++ sse/libs/workers/data.py
@@ -1,14 +1,20 @@
 from libs.structures import Worker
 from libs.consumer import consume_from_topic
 import pyding
+import logging
 
+logger = logging.getLogger(__name__)
 
 
 class DataGather(Worker):
     async def work(self):
         while True:
             async for message, id in consume_from_topic('positions'):
-                pyding.call('position.message', message=message, id=id, tracker_id=message['rastreador']['id'], **message)
+                try:
+                    pyding.call('position.message', message=message, id=id, tracker_id=message['rastreador']['id'], **message)
+                except KeyError:
+                    logger.critical("Failed to get data from kafka.")
+                    logger.critical(f"{message}")
 
     @classmethod
     def begin(cls):
