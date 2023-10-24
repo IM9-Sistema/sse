497ecff6de281ec1265073456269645385018af7
Jão <joao_bernardi@outlook.com>
2023-10-24 16:00:02 -0300

    `Connector` - Init

---
+++ volumes/connector/entrypoint.sh
@@ -0,0 +1,13 @@
+
+echo -e "\n\n=============\nWaiting for Kafka Connect to start listening on connector ⏳\n=============\n"
+while [ $(curl -s -o /dev/null -w %{http_code} http://connector:8083/connectors) -ne 200 ] ; do
+  echo -e "\t" $(date) " Kafka Connect listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://connector:8083/connectors) " (waiting for 200)"
+  sleep 5
+done
+echo -e $(date) "\n\n--------------\n\o/ Kafka Connect is ready! Listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://connector:8083/connectors) "\n--------------\n"
+
+# Now create your connector
+## Inline config example: 
+eval "cat <<EOF
+$(cat config.json)
+EOF" | curl -X POST -H "Content-Type: application/json" -d @- http://connector:8083/connectors
