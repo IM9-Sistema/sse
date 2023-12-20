d9882a305a2df79a5844388f958b1d858ba5f43d
Jão <joao_bernardi@outlook.com>
2023-12-20 03:54:39 -0300

    `SSE` - Use redis on auth

--- sse/libs/auth.py
+++ sse/libs/auth.py
@@ -5,6 +5,7 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt 
 from .utils import get_secret_key, get_sse_key, random_string
 from .structures import TokenData, Payload, SessionIdentifier, Session
+from .database import RedisCache
 
 ALGORITHM = 'HS256'
 oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -30,12 +31,14 @@ def get_current_user(token: Annotated[str, Depends(oauth_scheme)]):
         raise credentials_exception
 
 async def get_sse_session(x_sse_session: Annotated[str, Header()]) -> Session:
-    if x_sse_session not in sessions:
+    cache = RedisCache()
+    session = await cache.get(x_sse_session, Session)
+    if not session:
         raise HTTPException(
             status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect sse session."
         )
-    return sessions[x_sse_session]
+    return session
 
 async def create_sse_session(request: Request, session_identification: Annotated[Payload[SessionIdentifier], Body()], x_sse_key: Annotated[str, Header()]) -> Session:
     if x_sse_key != get_sse_key():
@@ -43,14 +46,16 @@ async def create_sse_session(request: Request, session_identification: Annotated
             status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect sse key."
         )
-        
+
+    redis = RedisCache()
+
     session_id = session_identification.data
     session = Session(
         session=session_id,
         id=uuid4(),
         token=random_string(128)
     )
-    sessions[session.token] = session
+    await redis.set(session.token, session)
     return session
 
 
