062ece679c9540942f3c94a7c48f204347aba9e0
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-03-22 01:59:21 -0300

    `SSE` - Fix healthcheck (syntax)

--- sse/healthcheck.py
+++ sse/healthcheck.py
@@ -1,10 +1,10 @@
 from os import environ
 import requests
 from requests.exceptions import Timeout
+from sys import exit
 
-if __name__ == "__main__":
-	try:
-		requests.get("http://127.0.0.1:8000", {"token", environ.get("SSE_USER_BYPASS_TOKEN", "")}, stream=True)
-		exit(0)
-	except:
-		exit(1)
+try:
+	requests.get("http://127.0.0.1:8000", {"token", environ.get("SSE_USER_BYPASS_TOKEN", "")}, stream=True)
+	exit(0)
+except:
+	exit(1)
