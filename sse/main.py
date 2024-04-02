49792fc8e4ea4532b392b4e4bd2a90a0a78a9f10
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-04-02 08:44:38 -0300

    `Elastic` - Init

--- sse/main.py
+++ sse/main.py
@@ -1,5 +1,7 @@
+from os import environ
 import sys, types
 
+# Some library shenanigans
 m = types.ModuleType('kafka.vendor.six.moves', 'Mock module')
 setattr(m, 'range', range)
 sys.modules['kafka.vendor.six.moves'] = m
@@ -11,12 +13,26 @@ from libs.workers.data import PositionGather, AlertsGather, ProcessAlerts, Proce
 from libs.workers.kafka_worker import KafkaPublish
 import logging
 import pyding
+from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.getLevelNamesMapping()[environ.get('LOG_LEVEL')])
 app = FastAPI()
+
+# Load routes dynamically
 for router in get_routers():
 	app.include_router(router)
 
+# Include Elastic's middleware
+apm = make_apm_client({
+    'SERVICE_NAME': 'FastAPI_SSE',
+    'SECRET_TOKEN': environ.get('ELASTIC_APM_SECRET_TOKEN'),
+    'SERVER_URL': environ.get('ELASTIC_APM_ADDRESS'),
+    'LOG_LEVEL': environ.get('LOG_LEVEL'),
+})
+
+app.add_middleware(ElasticAPM, client=apm)
+
+# Initialize Kafka Consumers
 PositionGather.begin()
 AlertsGather.begin()
 ProcessAlerts.begin()
