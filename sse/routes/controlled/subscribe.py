dbe514edec1bb49c32fb057f197a5fd713664b0f
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-07-05 10:43:13 -0300

    `Controlled` - Fix typos

--- sse/routes/controlled/subscribe.py
+++ sse/routes/controlled/subscribe.py
@@ -8,7 +8,7 @@ router = APIRouter(prefix='/events', tags=['controlled', 'events'])
 
 def handle(topic: str):
 	for data, id in consume_from_topic(topic):
-		yield f"id: {id}\nevent: message\ntopic: {topic}\ndata: {data}"
+		yield f"id: {id}\nevent: message\ntopic: {topic}\ndata: {data}\n\n"
 
 @router.get('/subscribe/{topic}')
 async def authenticate(topic: str, session: Session = Depends(get_sse_session)):
