d3b6fb1038c60e5648a63007886121f9329ec787
João Bernardi (Desktop) <joao_bernardi@outlook.com>
2024-10-14 19:24:19 -0300

    `SSE` - Health checker

--- sse/healthcheck.py
+++ sse/healthcheck.py
@@ -4,9 +4,12 @@ from requests.exceptions import Timeout
 from sys import exit
 
 try:
-	resp = requests.get("http://10.15.1.108:8000/positions/subscribe", {"token": environ.get("SSE_USER_BYPASS_TOKEN", "")}, stream=True)
-	assert resp.status_code == 200
-	exit(0)
+	for i in range(8000, 8010):
+		resp = requests.get(f"http://10.15.1.108:{i}/positions/subscribe", {"token": environ.get("SSE_USER_BYPASS_TOKEN", "")}, stream=True)
+		assert resp.status_code == 200
+		resp = requests.get(f"http://10.15.1.108:{i}/alerts/subscribe", {"token": environ.get("SSE_USER_BYPASS_TOKEN", "")}, stream=True)
+		assert resp.status_code == 200
+		exit(0)
 except Exception as e:
 	print(e)
 	exit(1)
